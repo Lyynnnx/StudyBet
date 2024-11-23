3a77fcac03618c14a726f72872a7e8c715829ece
LynokViktor <lynokviktor@gmail.com>
2024-11-23 20:25:03 +0100

    test

--- client/lib/screens/wallet_connection.dart
+++ client/lib/screens/wallet_connection.dart
@@ -14,18 +14,32 @@ class WalletConnection {
         "dapp_encryption_public_key": dappEncryptionPublicKey,
         "redirect_uri": "$redirectUriScheme://auth",
       });
+     // final uri2 = Uri.https("https://phantom.app/ul/v1/connect?dapp_encryption_public_key=<ваш_публичный_ключ>&redirect_uri=studybet.app://auth")
+      //final uri2 = Uri.https("https://phantom.app/ul/v1/connect?dapp_encryption_public_key=${publicKey}y&redirect_uri=studybet.app://auth");
+      //final uri2 = Uri.https("https://phantom.app/ul/v1/connect?dapp_encryption_public_key=<ваш_публичный_ключ>&redirect_uri=studybet.app://auth");
+      String uri2 = "https://phantom.app/ul/v1/connect?dapp_encryption_public_key=${publicKey}>&redirect_uri=studybet.app://auth";
+      print("Сформированный URI: $uri2");
 
       // Открываем Phantom Wallet для подключения
       final result = await FlutterWebAuth.authenticate(
-        url: uri.toString(),
+       // url: uri.toString(),
+         url: uri2,
         callbackUrlScheme: redirectUriScheme,
       );
+      print("Результат аутентификации: $result");
 
       // Получаем адрес кошелька из URL
-      final wallet = Uri.parse(result).queryParameters['public_key'];
+     final uri1 = Uri.parse(result);
 
-      // Возвращаем адрес кошелька
-      return wallet;
+// Проверяем, есть ли параметр `public_key` в URL
+  if (uri1.queryParameters.containsKey('public_key')) {
+    final wallet = uri.queryParameters['public_key'];
+    return wallet;
+  } else {
+  // Если параметр отсутствует, возвращаем null
+    print("Ошибка: параметр public_key отсутствует в URL");
+  return null;
+}
     } catch (e) {
       print("Ошибка подключения к Phantom Wallet: $e");
       return null;
